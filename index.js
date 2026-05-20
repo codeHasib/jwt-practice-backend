d29a668f28d985aced513ae84fac482943f6e2d9
codeHasib <mohammadhasib9827@gmail.com>
2026-05-20 21:49:01 +0600

    add db connection and get routes

--- index.js
+++ index.js
@@ -9,10 +9,49 @@ app.use(cors());
 
 // middleware
 const verify = (req, res, next) => {
-  console.log("verify mid is running");
   next();
 };
 
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = process.env.DB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("jwt-practice");
+    const userCollection = db.collection("users");
+
+    app.get("/", (req, res) => {
+      res.send("Hi this is CoDeHasib's JWT Practice backend");
+    });
+
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/users/:id", verify, async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      console.log(query);
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", verify, (req, res) => {
   res.send("Hello world");
